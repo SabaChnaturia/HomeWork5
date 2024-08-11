dadbb0dfcd614f6fc951ec9694be7120f9db7eee
SabaChnaturia <154993776+SabaChnaturia@users.noreply.github.com>
2024-08-11 21:49:03 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let temperature = -12;
+
+if(temperature < 0){
+    console.log("ყინავს!");
+}
+
+else if(temperature >= 0 && temperature <= 10){
+    console.log("ძალიან ცივა");
+}
+else if(temperature >=11 && temperature <= 20){
+    console.log("ცივა");
+}
+else if(temperature >= 21 && temperature <= 30){
+    console.log("თბილა");
+}
+else if(temperature >30){
+    console.log("ცხელა");
+}
